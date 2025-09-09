61f10084da0eeabed34d05ffc0f70bb1efcb9983
ElJoho <johalopezari@unal.edu.co>
2025-09-09 14:40:48 -0500

    Se añadieron notas de clase 62 asi como paquete nuevo de aduinobot_msgs

--- arduinobot_ws/build/arduinobot_py_examples/share/arduinobot_py_examples/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/joho/Documents/RoboticsAndROS2-LearnByDoingManipulators/arduinobot_ws/build/arduinobot_py_examples"
